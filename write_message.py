dab225393acf573101c6025580a3cfcc3dfe416e
jrdimande <timoteojrdimande@gmail.com>
2024-12-26 14:39:23 +0000

    ...

---
+++ write_message.py
@@ -0,0 +1,5 @@
+filepath = '/home/vboxuser/files-and-exceptions/txts/programming.txt'
+
+with open(filepath, 'w') as file_object:
+    file_object.write('I love programming.')
+    file_object.write('I love creating new games.')
