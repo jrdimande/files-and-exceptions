7bb4d23eff744b3ebd8e04aca381b47665d110fe
jrdimande <timoteojrdimande@gmail.com>
2024-12-30 23:57:26 +0000

    ...

---
+++ Storing Data/favorite_number.py
@@ -0,0 +1,7 @@
+import json
+filename = 'favorite_number.json'
+number = int(input("Enter your favorite number: "))
+
+with open(filename,'w') as f:
+    json.dump(number, f)
+    print("Your favorite numbers was saved")
---
+++ Storing Data/favorite_number_reader.py
@@ -0,0 +1,11 @@
+import json
+
+filename = 'favorite_number.json'
+
+try:
+    with open(filename) as f:
+        number = json.load(f)
+        print(f"Your favorite number is {number}")
+
+except FileNotFoundError:
+    print(f"The file {filename} does not exist")
