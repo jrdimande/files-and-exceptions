8af9d156d6eef26b820a2a29c6486ccf153a67d7
jrdimande <timoteojrdimande@gmail.com>
2024-12-30 14:59:35 +0000

    ...

--- exceptions/sum.py
+++ exceptions/sum.py
@@ -1,11 +1,11 @@
+while True:
+    try:
+        num1 = int(input('Enter the firs number: '))
+        num2 = int(input('Enter the second number: '))
 
-try:
-    num1 = int(input('Enter the firs number: '))
-    num2 = int(input('Enter the second number: '))
 
-
-except ValueError:
-    print('Invalid Input!')
-else:
-    sum = num1 + num2
-    print(f'sum = {sum}')
+    except ValueError:
+        break
+    else:
+        sum = num1 + num2
+        print(f'sum = {sum}')
