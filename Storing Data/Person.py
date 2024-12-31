0bbc21a392a288065301159f40a4cd00b3429d02
jrdimande <timoteojrdimande@gmail.com>
2024-12-31 00:13:30 +0000

    ...

---
+++ Storing Data/Person.py
@@ -0,0 +1,21 @@
+import json
+
+filename = 'personInfo.json'
+
+person = {}
+
+name = input("Enter your name: ")
+age = int(input("Enter your age: "))
+heigth = float(input("Enter your heigth: "))
+phone_number = int(input("Enter your phone number: "))
+
+person['name'] = name
+person['age'] = age
+
+
+person['heigth'] = heigth
+person['phone_number'] = phone_number
+
+with open(filename, 'a') as f:
+    json.dump(person, f)
+    print("Done!")
