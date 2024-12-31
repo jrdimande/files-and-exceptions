11caf54b8686efef69ea06981f4227d1115f6f35
jrdimande <timoteojrdimande@gmail.com>
2024-12-31 00:27:26 +0000

    ...

---
+++ Storing Data/teste.py
@@ -0,0 +1,13 @@
+import json
+
+filename = 'teste.json'
+info = {'user' : {}}
+
+info['user']['name'] = 'ian'
+info['user']['age'] = '14'
+info['user']['heigth'] = 1.80
+
+with open(filename, 'w') as f:
+    json.dump(info, f)
+    print('Done!')
+
---
+++ Storing Data/teste_reader.py
@@ -0,0 +1,10 @@
+import json
+
+filename = 'teste.json'
+
+with open(filename) as f:
+    content = json.load(f)
+
+print(content['user']['name'])
+print(content['user']['age'])
+print(content['user']['heigth'])
